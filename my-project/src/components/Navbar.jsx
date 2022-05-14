9bed80ab07c36bcf64ecc3c2253a49365af3bf1a
Stephen Wong <yummywongttv@gmail.com>
2022-05-14 09:33:50 -0400

    Update Navbar.jsx
    generated functional component using prefaced rafce

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,2 +1,11 @@
 //I use .jsx so i can use my html snippets, saves a bit more time//
 
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
